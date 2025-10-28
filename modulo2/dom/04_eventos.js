37ac92989ab3cb52e9fa98e0d776398eba9282ee
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-28 10:01:33 -0500

    poo

--- modulo2/dom/04_eventos.js
+++ modulo2/dom/04_eventos.js
@@ -1,10 +1,21 @@
-let parrafo=null;
+let parrafo= null;
 document.getElementById('crear').addEventListener('click',()=>{
     parrafo = document.createElement('p');
-    parrafo.textContent="Parrafo dinamico desde Javascript";
+    parrafo.textContent="Párrafo dinámico desde Javascript";
     document.getElementById('contenedor').appendChild(parrafo);
 });
 document.getElementById('eliminar').addEventListener('click',()=>{
-    if(parrafo)parrafo.remove();
-    
-});
+    if (parrafo) parrafo.remove();
+})
+
+function saludar(){
+    alert("Hola desde la función");
+}
+
+document.getElementById('parrafo').addEventListener('dbclick', ()=>{
+    alert("Hiciste doble click en el párrafo");
+})
+
+document.getElementById('nombre').addEventListener('input', (e)=>{
+    console.log("El valor del input es: ", e.target.value);
+})
